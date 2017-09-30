afcba0b2d55b583bae3704c5a5e2dfd95c8617f4
Rene Ladan <r.c.ladan@gmail.com>
2017-09-30 23:39:53 +0200

    Production fixes:
    - allow injector to live anywhere on the file system
    - create a ~/.sandsifter directory instead of ./data to store results

--- sifter.py
+++ sifter.py
@@ -27,10 +27,10 @@ import code
 import copy
 from ctypes import *
 
-INJECTOR = "./injector"
+INJECTOR = "injector"
 arch = ""
 
-OUTPUT = "./data/"
+OUTPUT = os.getenv("HOME") + "/.sandsifter/"
 LOG  = OUTPUT + "log"
 SYNC = OUTPUT + "sync"
 TICK = OUTPUT + "tick"
@@ -808,9 +808,16 @@ def main():
     if not os.path.exists(OUTPUT):
         os.makedirs(OUTPUT)
 
+    real_injector, errors = \
+        subprocess.Popen(
+                ['which', INJECTOR],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+                ).communicate()
+    real_injector = real_injector.replace('\n', '') # strip newline from shell output
     injector_bitness, errors = \
         subprocess.Popen(
-                ['file', INJECTOR],
+                ['file', real_injector],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE
                 ).communicate()
